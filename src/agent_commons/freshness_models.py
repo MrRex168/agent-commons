34fedca0ac5343f5a20045880501dfd2cd6121be
Suresh Menon <motiondynamic168@gmail.com>
2026-09-13 22:21:28 +0800

    Remove unused freshness model import

--- src/agent_commons/freshness_models.py
+++ src/agent_commons/freshness_models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from sqlalchemy import ForeignKey, Integer, String
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from agent_commons.db import Base
