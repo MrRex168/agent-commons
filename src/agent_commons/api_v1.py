2d272e288e68beb74a8e4e8df0d953aaeea9b6e4
Suresh Menon <motiondynamic168@gmail.com>
2026-09-14 18:16:05 +0800

    Register controller-signed state v3 routes

--- src/agent_commons/api_v1.py
+++ src/agent_commons/api_v1.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from agent_commons.agents import router as agents_router
 from agent_commons.communication import router as communication_router
+from agent_commons.controller_state import router as controller_state_router
 from agent_commons.discovery import router as discovery_router
 from agent_commons.freshness import router as freshness_router
 from agent_commons.lineage import router as lineage_router
@@ -17,6 +18,7 @@ router.include_router(rotation_router)
 router.include_router(recovery_router)
 router.include_router(lineage_router)
 router.include_router(freshness_router)
+router.include_router(controller_state_router)
 router.include_router(migration_router)
 router.include_router(continuity_router)
 router.include_router(communication_router)
